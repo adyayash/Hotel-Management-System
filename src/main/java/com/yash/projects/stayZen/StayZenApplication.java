0ad00aef7eb9f1f91e098209a12f45b818a50158
adyayash <yasminbanu5112@gmail.com>
2026-03-28 19:36:23 +0530

    Initital Commit

---
+++ src/main/java/com/yash/projects/stayZen/StayZenApplication.java
@@ -0,0 +1,13 @@
+package com.yash.projects.stayZen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayZenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayZenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/projects/stayZen/StayZenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.projects.stayZen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayZenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
